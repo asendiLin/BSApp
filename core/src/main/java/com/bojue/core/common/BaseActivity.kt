fce75917203fbb827f494b157f3678b47be980e9
asendiLin <a_sendi@163.com>
2019-05-06 22:36:01 +0800

    添加BaseActivity,为Activity提供公共的父类。

---
+++ core/src/main/java/com/bojue/core/common/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.bojue.core.common
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * author: asendi.
+ * data: 2019/5/6.
+ * description:
+ */
+open class BaseActivity : AppCompatActivity(){
+}
