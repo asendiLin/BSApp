42b2f5f7242786964262ef4e7febe47614f0a712
sendi <linbosen@52tt.com>
2019-05-08 21:12:14 +0800

    添加社区分享Fragment

---
+++ app/src/main/java/com/bojue/bsapp/community/CommunityFragment.kt
@@ -0,0 +1,20 @@
+package com.bojue.bsapp.community
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bojue.core.common.BaseFragment
+
+/**
+ * create data : 2019/5/8
+ * author : sendi
+ * description :社区Fagment
+ */
+class CommunityFragment : BaseFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+}
