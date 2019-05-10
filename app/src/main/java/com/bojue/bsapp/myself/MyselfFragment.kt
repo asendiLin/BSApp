bd87d5e9f861e1da8647a30f192d89647c081efd
asendiLin <a_sendi@163.com>
2019-05-10 18:34:01 +0800

    添加MyselfFragment.

---
+++ app/src/main/java/com/bojue/bsapp/myself/MyselfFragment.kt
@@ -0,0 +1,11 @@
+package com.bojue.bsapp.myself
+
+import com.bojue.core.common.BaseFragment
+
+/**
+ * author: asendi.
+ * data: 2019/5/10.
+ * description:
+ */
+class MyselfFragment :BaseFragment() {
+}
