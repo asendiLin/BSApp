e2ab88146beda1a0fb3f76e88ac0cedc42e7c5a1
asendiLin <a_sendi@163.com>
2019-05-12 12:42:40 +0800

    增加发布动态的activity

---
+++ app/src/main/java/com/bojue/bsapp/community/PublishCommunityActivity.kt
@@ -0,0 +1,13 @@
+package com.bojue.bsapp.community
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.bojue.bsapp.R
+
+class PublishCommunityActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_publish_community)
+    }
+}
---
+++ app/src/main/java/com/bojue/bsapp/community/PublishCommunityFragment.kt
@@ -0,0 +1,21 @@
+package com.bojue.bsapp.community
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bojue.core.common.BaseFragment
+
+/**
+ * author: asendi.
+ * data: 2019/5/12.
+ * description:
+ */
+class PublishCommunityFragment :BaseFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+}
