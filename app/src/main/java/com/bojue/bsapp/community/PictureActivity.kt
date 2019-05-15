de51b0c065eb6e26a62d7e28a383202938b710a4
asendiLin <a_sendi@163.com>
2019-05-15 13:02:19 +0800

    增加底部照片选择弹框。

---
+++ app/src/main/java/com/bojue/bsapp/community/PictureActivity.kt
@@ -0,0 +1,47 @@
+package com.bojue.bsapp.community
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.bojue.bsapp.R
+import com.bojue.bsapp.util.MediaLoader
+import com.yanzhenjie.album.Album
+import com.yanzhenjie.album.AlbumConfig
+import android.util.Log
+import com.yanzhenjie.album.AlbumFile
+
+
+class PictureActivity : AppCompatActivity() {
+
+    private val myTag = "PictureActivity"
+
+    private val mAlbumFiles = ArrayList<AlbumFile>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_picture)
+
+        //TODO:初始化Album
+        Album.initialize(AlbumConfig.newBuilder(this)
+                .setAlbumLoader(MediaLoader())
+                .build())
+    }
+
+    fun onSelect(view :View){
+        Album.image(this) // Image selection.
+                .singleChoice()
+                .camera(true)
+                .columnCount(3)
+//                .filterSize() // Filter the file size.
+//                .filterMimeType() // Filter file format.
+//                .afterFilterVisibility() // Show the filtered files, but they are not available.
+                .onResult { albumFileList ->
+                    albumFileList.listIterator().forEach {
+                        Log.i(myTag,"path : ${it.path}")
+                    }
+                }
+                .onCancel{ cancel ->
+                }
+                .start()
+    }
+}
--- app/src/main/java/com/bojue/bsapp/community/PublishCommunityActivity.kt
+++ app/src/main/java/com/bojue/bsapp/community/PublishCommunityActivity.kt
@@ -1,7 +1,12 @@
 package com.bojue.bsapp.community
 
+import android.app.Dialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.WindowManager
 import com.bojue.bsapp.R
 
 class PublishCommunityActivity : AppCompatActivity() {
@@ -9,5 +14,24 @@ class PublishCommunityActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_publish_community)
+
+    }
+
+    fun onGetPhoto(view : View){
+        showBottomDialog()
+    }
+
+
+    private fun showBottomDialog(){
+        val bottomDialog = Dialog(this,R.style.BottomDialog)
+        val view = LayoutInflater.from(this).inflate(R.layout.dialog_select_bottom,null,false)
+        bottomDialog.setContentView(view)
+        val window = bottomDialog.window
+        window.setGravity(Gravity.BOTTOM)
+        val params = window.attributes
+        params.width = WindowManager.LayoutParams.MATCH_PARENT
+        params.height = WindowManager.LayoutParams.WRAP_CONTENT
+        window.attributes = params
+        bottomDialog.show()
     }
 }
