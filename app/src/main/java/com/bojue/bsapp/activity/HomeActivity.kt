bec501f6d8a1d410e32cb1749e24612c631d3317
asendiLin <a_sendi@163.com>
2019-05-15 22:23:46 +0800

    修复主页面中页面切换不了的bug.

--- app/src/main/java/com/bojue/bsapp/activity/HomeActivity.kt
+++ app/src/main/java/com/bojue/bsapp/activity/HomeActivity.kt
@@ -65,6 +65,7 @@ class HomeActivity : BaseActivity() {
             }else{
                 transaction.show(toFragment).commit()
             }
+            mCurrentFragment = toFragment
         }
     }
 
